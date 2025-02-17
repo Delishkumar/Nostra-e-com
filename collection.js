ce26f9a52a10ce4756b43510572021aa91f615cd
Delishkumar <delishkumar800@gmail.com>
2025-02-17 12:59:38 +0530

    js update

--- collection.js
+++ collection.js
@@ -17,9 +17,56 @@ cut.addEventListener("click",function(){
     slidemenu.style.display="none"
 })
 
+const sbutn = document.getElementById("sbutn")
+let inputbox = document.getElementById("search")
+var con = document.querySelector(".shirts")
+var divlist = con.querySelectorAll("div")
 
-    
-    
+sbutn.addEventListener("click",function(){
+  var entervalue = search.value.toUpperCase()
+  for(count = 0; count<divlist.length; count = count+1){
+
+    var h6text = divlist[count].querySelector("h6").textContent
+    if(h6text.toUpperCase().indexOf(entervalue)<0){
+     divlist[count].style.display="none"
+    }
+    else{
+       divlist[count].style.display="inline"
+    }
+  }
+})
+  
+
+document.getElementsByName("check").forEach( checkbox => { 
+  var checkvalue = checkbox.value.toUpperCase()
+  var container = document.querySelector(".shirts")
+var divlists = container.querySelectorAll("div")
+
+checkbox.addEventListener("click",function(){
+  for(i = 0; i<divlists.length; i = i+1){
+    var h6list = divlists[i].querySelector("h6").textContent
+    if(checkbox.checked){
+      console.log("clicked")
+      if(h6list.toUpperCase().indexOf(checkvalue)>0){
+          divlists[i].style.display="inline"
+
+      }
+    else{
+      divlists[i].style.display="none"
+    }
+  
+    }
+    else{
+      console.log("uncheck")
+     divlist[i].style.display="inline"
+    }
+  }
+
+})
+  
+  
+});
+
     
    
     
